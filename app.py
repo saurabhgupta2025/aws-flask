39a1eb81bfb5706d071d4a4d9d4793f75aa974e2
iampsrv <iampsrv@gmail.com>
2023-04-11 14:26:31 +0530

    added s3

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from upload import *
 
 app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello Everyone"
+
+@app.route("/upload", methods=['POST', 'GET'])
+def upload():
+        if request.method == 'POST':
+            f = request.files['file']
+            print(f)
+            file_name = upload_my_file(f)
+            print(file_name)
+       
+        return render_template('uploads3.html')
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=8080,debug=True)
