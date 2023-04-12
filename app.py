71111498189928d827dfbec5bf2bae41665acb34
iampsrv <iampsrv@gmail.com>
2023-04-12 20:23:21 +0530

    modified app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request
 from upload import *
+import socket
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "Hello Everyone"
+    return "Hello Everyone, this app is served from {}".format(socket.gethostname())
 
 @app.route("/upload", methods=['POST', 'GET'])
 def upload():
